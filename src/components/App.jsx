230147ac49e08f47b58721c627297c789202b8e1
DimaKrupenko <dimakrupenko77@gmail.com>
2023-05-14 21:44:13 +0300

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { lazy, Suspense } from 'react';
 
 const Home = lazy(() => import('../pages/Home'));
